eff8247caffbcad3ea4c6b24567129073fab37c3
vostrik <iravost@yandex.ru>
2023-01-10 21:51:04 +0300

    task_1 changed

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,19 +1,20 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
+  
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
+    let message = productName "за" productPrice "теперь в корзине!"; // создайте переменную message
     // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
+   // добавьте в конец переменной message пробел
+   // добавьте в конец переменной message текст 'за'
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message значение переменной productPrice
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
-// какой-то комментарий
+
+    console.log(message);
     // Конец решения задания №1.1.
 
     return message;
